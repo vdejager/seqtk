4bca4c6ad8b1e1e47ebc060218d1db8872b17f7c
vdejager <vdejager@micevo-78.wurnet.nl>
2022-03-16 13:42:28 +0100

    added fastq support for subseq along with feature to include the bedfile name column

--- seqtk.c
+++ seqtk.c
@@ -1947,7 +1947,7 @@ static int usage()
 {
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Usage:   seqtk <command> <arguments>\n");
-	fprintf(stderr, "Version: 1.3-r117-dirty\n\n");
+	fprintf(stderr, "Version: 1.3-r119-dirty\n\n");
 	fprintf(stderr, "Command: seq       common transformation of FASTA/Q\n");
 	fprintf(stderr, "         comp      get the nucleotide composition of FASTA/Q\n");
 	fprintf(stderr, "         sample    subsample sequences\n");
